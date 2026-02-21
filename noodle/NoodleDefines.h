b6ff69dd76e1343d9caec92c11c21e4f71d5949a
Eric Weubbe <EJWeubbe@student.fullsail.edu>
2026-02-21 12:16:46 -0600

    Adding constexpr for common memory units

--- noodle/NoodleDefines.h
+++ noodle/NoodleDefines.h
@@ -8,3 +8,18 @@ typedef std::uint64_t uint64;
 typedef std::int8_t int8;
 typedef std::int32_t int32;
 typedef std::int64_t int64;
+
+constexpr size_t operator"" _KB(unsigned long long x)
+{
+    return x * 1024ull;
+}
+
+constexpr size_t operator"" _MB(unsigned long long x)
+{
+    return x * 1024ull * 1024ull;
+}
+
+constexpr size_t operator"" _GB(unsigned long long x)
+{
+    return x * 1024ull * 1024ull * 1024ull;
+}
