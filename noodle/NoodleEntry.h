04dab885366973fc4bb3b1f18c46301fdc067e96
Eric Weubbe <EJWeubbe@student.fullsail.edu>
2026-02-25 23:08:50 -0600

    Adding clarifying comments to platform interface

--- noodle/NoodleEntry.h
+++ noodle/NoodleEntry.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Core/NoodleCore.h"
 
+// Platform functions should be implemented by platform entry point.
+// See WinEntry.cpp for examples
 bool PlatformCreateWindow(const NoodleWindowDesc& windowDesc);
 void PlatformInitLogger();
 void PlatformDispatchMessages();
