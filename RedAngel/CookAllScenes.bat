e336917fb4b7437847b8ff895eca25a1601c0395
Eric Weubbe <eweubbe8@gmail.com>
2026-05-19 17:04:25 -0500

    Adding quick script to cook all scenes.

---
+++ RedAngel/CookAllScenes.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set APP_ROOT=%~dp0
+set ASSET_TYPE=scene
+set SCENE_DIR=%APP_ROOT%\Assets\Scene
+set COOKER=%APP_ROOT%..\noodle\Lib\x64\Debug\noodleCooker.exe
+
+for /R %SCENE_DIR% %%f in (*.json) do (
+	echo Cooking %%f
+	"%COOKER%" %ASSET_TYPE% %APP_ROOT% %%~nf
+)
+
+echo Done.
+pause
