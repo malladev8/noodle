2226d924a3aaf93b94681e8351d1f0c98a38c259
Eric Weubbe <eweubbe8@gmail.com>
2026-07-06 18:33:24 -0500

    Shader cooking now uses correct dxc relative path. Adding required dxc files to repository.

--- noodleCooker/Source/ShaderCooker.cpp
+++ noodleCooker/Source/ShaderCooker.cpp
@@ -1,8 +1,10 @@
 #include "ShaderCooker.h"
+#include <filesystem>
 
 bool ShaderCooker::CookShaderByteCode(const std::string& sourcePath, const std::string& entry, const std::string& profile, const std::string& outputPath)
 {
-    std::string command = "B:/ScarlettDev/RA/noodle/Source/ThirdParty/dxc/dxc_2026_05_27/bin/x64/dxc.exe " + sourcePath + " -E " + entry + " -T " + profile + " -Fo " + outputPath;
-    system(command.c_str());
-	return true;
+    std::filesystem::path dxcPath = std::filesystem::current_path().parent_path() / "noodle/Source/ThirdParty/dxc/dxc_2026_05_27/bin/x64/dxc.exe";
+    std::string command = dxcPath.string() + " " + sourcePath + " -E " + entry + " -T " + profile + " -Fo " + outputPath;
+    int result = system(command.c_str());
+    return result == 0;
 }
